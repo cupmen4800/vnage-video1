5f6cc02b2f5507023e817c39d80155bbed185b31
cupmen <cupmen@cupmen-macbook-air.local>
2022-12-25 20:18:51 +0900

    iroiro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React, { useRef, useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
 import '@vonage/video-publisher/video-publisher.js';
 import '@vonage/video-subscribers/video-subscribers.js';
 import { HiVideoCamera, HiVideoCameraSlash } from "react-icons/hi2"
 import { IoIosMicOff, IoIosMic } from "react-icons/io"
 
-function App() {
+// APIキーたちを.env.localから読み込み
+const VONAGE_API_KEY = process.env.REACT_APP_VONAGE_API_KEY
+const VONAGE_SESSION_ID = process.env.REACT_APP_VONAGE_SESSION_API
+const VONAGE_TOKEN = process.env.REACT_APP_VONAGE_TOKEN
+
+const App = () => {
+  // 切り替えボタンの表示管理
   const [videoState, setVideoState] = useState(true)
   const [audioState, setAudioState] = useState(true)
-  // Get references to Web Components
+  // Web Componentsを参照
   const publisher = useRef(null);
   const subscribers = useRef(null);
-
-  // These values normally come from the backend in a production application, but for this demo, they are hardcoded
-  const apiKey = process.env.REACT_APP_VONAGE_API_KEY
-  const sessionId = process.env.REACT_APP_VONAGE_SESSION_API
-  const token = process.env.REACT_APP_VONAGE_TOKEN
 
   const toggleVideo = () => {
     setVideoState(!videoState)
@@ -32,38 +31,32 @@ function App() {
   useEffect(() => {
     const OT = window.OT;
 
-    // Initialize an OpenTok Session object
-    const session = OT.initSession(apiKey, sessionId);
+    // Opentokセッションの初期化
+    const session = OT.initSession(VONAGE_API_KEY, VONAGE_SESSION_ID);
 
-    // Set session and token for Web Components
+    // Web Componentsのセッションとトークンの設定
     publisher.current.session = session;
-    publisher.current.token = token;
+    publisher.current.token = VONAGE_TOKEN;
     subscribers.current.session = session;
-    subscribers.current.token = token;
-
+    subscribers.current.token = VONAGE_TOKEN;
   });
 
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header> */}
-      <div className="App-container">
-        <section className="App-section-publisher">
-          <video-subscribers width="35svh" ref={subscribers}></video-subscribers>
-        </section>
-        <section class="App-section-subscribers">
-          <video-publisher width="20vw" height="15svh" ref={publisher}></video-publisher>
-        </section>
-        <section className="App-section-tools">
-          <button onClick={toggleVideo}>
-            {videoState ? <HiVideoCamera /> : <HiVideoCameraSlash />}
-          </button>
-          <button onClick={toggleAudio}>
-            {audioState ? <IoIosMic /> : <IoIosMicOff />}
-          </button>
-        </section>
-      </div>
+      <section className="App-section-subscribers">
+        <video-subscribers width="37.5vw" height="37.5svh" ref={subscribers} />
+      </section>
+      <section class="App-section-publisher">
+        <video-publisher width="20vw" height="15svh" ref={publisher} />
+      </section>
+      <section className="App-section-tools">
+        <button onClick={toggleVideo}>
+          {videoState ? <HiVideoCamera /> : <HiVideoCameraSlash />}
+        </button>
+        <button onClick={toggleAudio}>
+          {audioState ? <IoIosMic /> : <IoIosMicOff />}
+        </button>
+      </section>
     </div>
   );
 }
